f5a4647c731d8f71c77c4e9de7b8814100fc411a
Bumpfel <34800245+Bumpfel@users.noreply.github.com>
2019-05-08 00:22:25 +0200

    responsive design

--- src/main/js/view/app.js
+++ src/main/js/view/app.js
@@ -12,22 +12,36 @@ class App extends React.Component {
         return (
             <div>
                 <div class="topbar">
-                    <div class="menu">
+                    {/* dropdown menu (normally hidden) */}
+                    <div class="dropdown-button">
+                        <i class="fa fa-bars"></i>
+                        <div class="dropdown-menu">
+                            <a>menu item #1</a>
+                            <a>menu item #2</a>
+                            <a>menu item #3</a>
+                        </div>
+                    </div>
+
+                    <div class="fluid-container">
                         <div class="logo">
                             <i class="fas fa-feather"></i>
                             thesis mgmt
                         </div>
-                        <a>menu item #1</a>
-                        <a>menu item #2</a>
-                        <a>menu item #3</a>
+                        {/* default menu */}
+                        <div class="menu">
+                            <a>menu item #1</a>
+                            <a>menu item #2</a>
+                            <a>menu item #3</a>
 
-                        <select class="role">
-                            <option>Role: Student</option>
-                            <option>Role: Opponent</option>
-                        </select>
+                            <select class="role">
+                                <option>Role: Student</option>
+                                <option>Role: Opponent</option>
+                            </select>
+                        </div>
                     </div>
                 </div>
-                <div class="content">
+                {/*  Content */}
+                <div class="fluid-container content">
                     Thesis status: N/A
                 </div>
             </div>
