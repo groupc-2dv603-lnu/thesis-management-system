2496aedb51740af1897f6ac6aa3a05b62c9a2029
Calle <Calle@example.com>
2019-05-08 14:25:16 +0200

    added GET to all users

--- src/main/java/project/controller/UsersController.java
+++ src/main/java/project/controller/UsersController.java
@@ -3,7 +3,11 @@ package project.controller;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.Resources;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,11 +30,22 @@ class UsersController {
 		Users user = repository.findFirstById(id);
 		
 		return new Resource<>(user,
-			    linkTo(methodOn(UsersController.class).one(id)).withSelfRel());//,
-			    //linkTo(methodOn(UsersController.class).all()).withRel("employees"));
+			    linkTo(methodOn(UsersController.class).one(id)).withSelfRel(),
+			    linkTo(methodOn(UsersController.class).all()).withRel("employees"));
 		
 	}
 	
+	@GetMapping("/users")
+	Resources<Resource<Users>> all() {
+		List<Resource<Users>> users = repository.findAll().stream()
+			    .map(employee -> new Resource<>(employee,
+			    		linkTo(methodOn(UsersController.class).one(employee.getId())).withSelfRel(),
+			    		linkTo(methodOn(UsersController.class).all()).withRel("users")))
+			    	    .collect(Collectors.toList());
+
+		return new Resources<>(users,
+				linkTo(methodOn(UsersController.class).all()).withSelfRel());
+	}
 	@PostMapping("/users")
 	public void postUsers() {
 		repository.save(new Users("Calle Johansson", "hejsan123"));
