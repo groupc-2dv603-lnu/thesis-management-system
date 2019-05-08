0347615c61e90f2034e87181d014abb639109784
jeppan875 <jeppan875@gmail.com>
2019-05-08 19:41:55 +0200

    setup hashrouter

--- src/main/js/view/app.js
+++ src/main/js/view/app.js
@@ -1,94 +1,41 @@
 'use strict';
 
-const React = require('react');
+import React, { Component } from 'react'
 const ReactDOM = require('react-dom');
 const client = require('./client');
+import { Route, Switch, HashRouter } from 'react-router-dom'
+import FrontPage from './Pages/FrontPage'
+import Student from './Pages/Student'
+import Coordinator from './Pages/Coordinator'
 
-class App extends React.Component {
+class App extends Component {
     constructor(props) {
         super(props);
-        this.state = {users: []};
+        this.state = { users: [] };
     }
 
     componentDidMount() {
-		client({method: 'GET', path: '/users'}).then(response => {
-			this.setState({users: response.entity._embedded.userses});
-		});
-	}
+        client({ method: 'GET', path: '/users' }).then(response => {
+            this.setState({ users: response.entity._embedded.userses });
+        });
+    }
 
     render() {
         return (
             <div>
-                <div class="topbar">
-                    {/* dropdown menu (normally hidden) */}
-                    <div class="dropdown-button">
-                        <i class="fa fa-bars"></i>
-                        <div class="dropdown-menu">
-                            <a>menu item #1</a>
-                            <a>menu item #2</a>
-                            <a>menu item #3</a>
-                        </div>
-                    </div>
-
-                    <div class="fluid-container">
-                        <div class="logo">
-                            <i class="fas fa-feather"></i>
-                            thesis mgmt
-                        </div>
-                        {/* default menu */}
-                        <div class="menu">
-                            <a>menu item #1</a>
-                            <a>menu item #2</a>
-                            <a>menu item #3</a>
-
-                        </div>
-                        <select class="role">
-                            <option>Role: Student</option>
-                            <option>Role: Opponent</option>
-                        </select>
-                    </div>
-                </div>
-                {/*  Content */}
-                <div class="fluid-container content">
-                    {/* Thesis status: N/A */}
-                    <UserList users={this.state.users}/>
-                </div>
+                <HashRouter >
+                    <Switch>
+                        <Route exact path='/' component={FrontPage} />
+                        <Route path='/student' component={Student} />
+                        <Route path='/coordinator' component={Coordinator} />
+                    </Switch>
+                </HashRouter>
             </div>
         )
     }
 }
 
-class UserList extends React.Component{
-	render() {
-		const users = this.props.users.map(user =>
-			<User key={user._links.self.href} user={user}/>
-		);
-		return (
-            <table>
-				<tbody>
-					<tr>
-						<td><strong>Name</strong></td>
-						<td><strong>Password</strong></td>
-					</tr>
-					{users}
-				</tbody>
-			</table>
-		)
-	}
-}
-
-class User extends React.Component {
-    render() {
-        return (
-            <tr>
-                <td>{this.props.user.name}</td>
-                <td>{this.props.user.password}</td>
-            </tr>
-        )
-    }
-}
-
 ReactDOM.render(
-	<App />,
-	document.getElementById('react')
+    <App />,
+    document.getElementById('react')
 )
