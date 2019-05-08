43ab6b51f02426047ecca7e5f6fe61351f3e4631
Bumpfel <34800245+Bumpfel@users.noreply.github.com>
2019-05-08 18:48:40 +0200

    fixed name convention

--- src/main/js/view/app.js
+++ src/main/js/view/app.js
@@ -11,36 +11,36 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <div class="topbar">
+                <div className="topbar">
                     {/* dropdown menu (normally hidden) */}
-                    <div class="dropdown-button">
-                        <i class="fa fa-bars"></i>
-                        <div class="dropdown-menu">
+                    <div className="dropdown-button">
+                        <i className="fa fa-bars"></i>
+                        <div className="dropdown-menu">
                             <a>menu item #1</a>
                             <a>menu item #2</a>
                             <a>menu item #3</a>
                         </div>
                     </div>
-                    <div class="fluid-container">
-                        <div class="logo">
-                            <i class="fas fa-feather"></i>
+                    <div className="fluid-container">
+                        <div className="logo">
+                            <i className="fas fa-feather"></i>
                             thesis mgmt
                         </div>
                         {/* default menu */}
-                        <div class="menu">
+                        <div className="menu">
                             <a>menu item #1</a>
                             <a>menu item #2</a>
                             <a>menu item #3</a>
 
                         </div>
                     </div>
-                    <select class="role">
+                    <select className="role">
                         <option>Role: Student</option>
                         <option>Role: Opponent</option>
                     </select>
                 </div>
                 {/*  Content */}
-                <div class="fluid-container content">
+                <div className="fluid-container content">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas tincidunt augue ut nibh facilisis, in lobortis neque iaculis. Donec at mauris euismod, placerat mi et, consequat quam. Nullam eu metus ut leo consequat sollicitudin porttitor eget sapien. Suspendisse potenti. Vestibulum in velit quis augue congue fringilla id ut dolor. Curabitur lobortis scelerisque dui at tempus. In euismod purus ut finibus semper.
                     <br /><br />
                     Etiam suscipit quam justo, et viverra arcu maximus a. Vestibulum tristique diam ante, eget pellentesque augue ultricies id. Donec at tincidunt leo, sit amet gravida mauris. Cras laoreet purus vel vestibulum pretium. Aliquam placerat diam ac turpis scelerisque, a sagittis nisi eleifend. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam lobortis elementum aliquam. Integer scelerisque vestibulum velit, nec fringilla tortor posuere nec. Proin vitae urna sodales, egestas est ac, hendrerit mi. Aenean quis magna sit amet ipsum imperdiet efficitur id eu tellus. Mauris lacinia ex id congue tempor. Quisque varius massa vel urna facilisis, quis posuere diam volutpat.
