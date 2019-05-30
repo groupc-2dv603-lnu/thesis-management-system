6dcc82f9639cc65df8369ba1616e1ee6b2907404
TimmeB <38351091+TimmeB@users.noreply.github.com>
2019-05-30 15:25:36 +0200

    upload file version2. yet only to local storage

--- src/main/java/project/ProjectApplication.java
+++ src/main/java/project/ProjectApplication.java
@@ -2,10 +2,13 @@ package project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import project.property.FileStorageProperties;
 
 @SpringBootApplication
 @EnableMongoAuditing                    //Enables use of @CreatedDate annotation in Submission class
+@EnableConfigurationProperties(FileStorageProperties.class)
 public class ProjectApplication {
 
     public static void main(String[] args) {
