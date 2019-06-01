278a2fbf2024e6cf3c896237d94e0bdfd0c9786a
jeppan875 <jeppan875@gmail.com>
2019-06-01 19:35:28 +0200

    fr

--- src/main/js/view/Pages/Reader/index.js
+++ src/main/js/view/Pages/Reader/index.js
@@ -3,55 +3,6 @@
 import React, { Component } from "react";
 import { postToAPI, getFromAPI, putToAPI } from "../../functions";
 
-const mockState = {
-  reports: [
-    {
-      id: "12348",
-      name: "report1",
-      author: "Nils Carlsson",
-      downloadUrl: "www.test2422.se"
-    },
-    {
-      id: "123488",
-      name: "report2",
-      author: "Anders Carlsson",
-      downloadUrl: "www.test244.se"
-    },
-    {
-      id: "123477",
-      name: "report3",
-      author: "Adam Svensson",
-      downloadUrl: "www.test4332.se"
-    },
-    {
-      id: "123465",
-      name: "report4",
-      author: "Axel Carlsson",
-      downloadUrl: "www.test343.se"
-    },
-    {
-      id: "123457",
-      name: "report5",
-      author: "Örjan Carlsson",
-      downloadUrl: "www.test433.se"
-    }
-  ],
-  selectedReports: [],
-  requestedReports: false,
-  initialReport: {
-    id: "1253457",
-    name: "report5",
-    author: "Örjan Carlsson",
-    downloadUrl: "www.test433.se"
-  },
-  finalReport: {
-    id: "1253457",
-    name: "report5",
-    author: "Örjan Carlsson",
-    downloadUrl: "www.test433.se"
-  }
-};
-
 class Reader extends Component {
   constructor(props) {
     super(props);
