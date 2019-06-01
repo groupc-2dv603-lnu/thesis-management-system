98997b5a054dfb5c45d0a4d10d33de6c753a604c
TimmeB <38351091+TimmeB@users.noreply.github.com>
2019-06-01 15:31:13 +0200

    removed SubmissionStatus enum and variable in Submission

--- src/main/java/project/controller/CoordinatorController.java
+++ src/main/java/project/controller/CoordinatorController.java
@@ -7,8 +7,6 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import project.model.entities.*;
 import project.model.DTOs.SubmissionsDTO;
-import project.model.enums.Grade;
-import project.model.enums.SubmissionStatus;
 import project.model.repositories.*;
 
 import javax.validation.Valid;
@@ -126,15 +124,5 @@ public class CoordinatorController {
         return new Resources<>(submissions,
                 linkTo(methodOn(CoordinatorController.class).getAllSubmissions()).withSelfRel());
     }
-
-    /* Update the status of a submission */
-    @PutMapping("/coordinator/submissions/{id}/updateStatus")
-    Submission updateSubmissionStatus(@PathVariable String id, @RequestParam SubmissionStatus newStatus){
-        Submission updatedSubmission = submissionRepository.findFirstById(id);
-        updatedSubmission.setSubmissionStatus(newStatus);
-        submissionRepository.save(updatedSubmission);
-
-        return updatedSubmission;
-
-    }
+    
 }
