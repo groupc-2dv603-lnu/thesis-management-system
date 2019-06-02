e1c2a0723760a1f1bae43c000a19303a5c29ed0c
Samuel Bäckström <sbackstrom95@gmail.com>
2019-06-02 21:18:30 +0200

    fixed opponent frontend

--- src/main/js/view/Pages/Opponent/index.js
+++ src/main/js/view/Pages/Opponent/index.js
@@ -19,16 +19,18 @@ class Opponent extends Component {
     });
     getFromAPI("/opponent/initailReportSubmission").then(info => {
       console.log(info, "uhigu");
+      console.log("Filename: " + info.entity.filename);
+      console.log("Entity: " + info.entity);
       this.setState({
-        finalReport: info.entity.finalReportId
+        finalReport: info.entity
       });
     });
   }
 
   sendFinalReport() {
     postToAPI(
-      `/opponent/feedbackFinalReport?text=${
-        document.getElementById("finalreport").value
+      `/opponent/feedback?text=${
+        document.getElementById("initial report").value
       }`
     );
   }
@@ -39,14 +41,14 @@ class Opponent extends Component {
         {!!this.state.finalReport && (
           <div>
             <p>
-              Final report: <span>Download</span>
+              Initial report: <span>Download</span>
             </p>
             <p>
-              {`${this.state.finalReport.name}, ${
+              {`${this.state.finalReport.filename}, ${
                 this.state.finalReport.author
               }`}
             </p>
-            <textarea rows="10" cols="70" id="finalreport" />
+            <textarea rows="10" cols="70" id="initial report" />
             <div>
               <button onClick={this.sendFinalReport.bind(this)}>Skicka</button>
             </div>
