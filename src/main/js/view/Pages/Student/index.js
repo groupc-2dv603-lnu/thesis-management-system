14c85e6aa594927764542d348b5385cf1adcb362
Bumpfel <34800245+Bumpfel@users.noreply.github.com>
2019-06-03 13:16:50 +0200

    fixed student page update and misc

--- src/main/js/view/Pages/Student/index.js
+++ src/main/js/view/Pages/Student/index.js
@@ -1,12 +1,8 @@
-// ½ sidan renderas inte om efter supervisor request - fixat, men fullösning. passar referens till supervisorbox och kör en uppdateringsmetod på den
-// ½ sidan uppdateras inte efter filuppladdning
-
 'use strict';
 
 import React, { Component } from 'react';
 import SupervisorBox from './supervisor';
 import Submission from './submission';
-import * as common from './../../functions'
 import './style.css';
 
 class Student extends Component {
@@ -19,20 +15,11 @@ class Student extends Component {
     componentDidMount() {
         const reportCalls = ["projectDescription", "projectPlan", "initialReport", "finalReport"];
 
-        let reports = new Array(4);
-        let count = 0;
-
-        for (let i = 0; i < reportCalls.length; i++) {
-            common.getFromAPI("/student/" + reportCalls[i]).then(report => {
-                reports[i] = <Submission reference={this} key={reportCalls[i]} reportData={report.entity} type={reportCalls[i]} />
-            })
-                .then(() => {
-                    count++;
-                    if (count == 4) {
-                        this.setState({ submissions: reports });
-                    }
-                })
-        };
+        let tempSubmissions = [];
+        reportCalls.forEach(reportCall => {
+            tempSubmissions.push(<Submission key={reportCall} type={reportCall} />);
+        })
+        this.setState({ submissions: tempSubmissions})
     }
 
     render() {
